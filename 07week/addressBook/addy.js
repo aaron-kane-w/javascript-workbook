640288416411b3446f4db032fcb831fffe1fe2f5
aaron-kane-w <aaron.kane.w@gmail.com>
2020-05-08 19:45:06 -0500

    working on address book

--- 07week/addressBook/addy.js
+++ 07week/addressBook/addy.js
@@ -1,33 +1,43 @@
 
 
 window.onload = function() {
-
   getPosts()
-  moreInfo()
 }
 
 function getPosts() {
   fetch('https://randomuser.me/api/?results=20')
   .then((response) => response.json()) 
   .then((posts) => { //posts represents the array of objects
-    let results = posts.results
-    let info = posts.info
-    let output = '<h2>Contacts:</h2>'
-
-    console.log('Results array', posts.results)
-    console.log('Info array', posts.info)
-
-    results.forEach(function(x) {
-      output += `
-      <div>
-        <h4>${x.name.first} ${x.name.last}</h4>
-        <p>${x.phone}</p>
-        <button class="btn">More Info</button>
-      </div>`
-    })
-    document.getElementById('contacts').innerHTML = output 
+    generateContacs(posts.results)
   })
 }
+
+function generateContacs(json) {
+  let output = '<h2>Contacts:</h2>'
+
+  console.log('Results array', json)
+
+  json.forEach(function(x) {
+    output += `
+    <div>
+      <h4>${x.name.first} ${x.name.last}</h4>
+      <p>${x.phone}</p>
+      <button class="btn">More Info</button>
+    </div>`
+  })
+  document.getElementById('contacts').innerHTML = output 
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 function moreInfo() {
@@ -35,4 +45,7 @@ function moreInfo() {
   
 }
 
+let btn = document.querySelectorAll('button').addEventListener('click', function() {
+  console.log('button clicked')
+})
 
