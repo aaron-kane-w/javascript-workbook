d5d556154e83a541ced5ee26fe494f42e043afd0
aaron-kane-w <aaron.kane.w@gmail.com>
2020-05-10 13:11:19 -0500

    finished address book project

--- 07week/addressBook/addy.js
+++ 07week/addressBook/addy.js
@@ -1,36 +1,69 @@
-
-
 window.onload = function() {
   getPosts()
 }
+const mainDiv = document.getElementById('contacts')
+const url = 'https://randomuser.me/api/?results=20'
+
+function createNode(elem) {
+  // Create the type of element you pass in the parameters
+  return document.createElement(elem)
+}
+
+function append(parent, child) {
+  // Append the second parameter(element) to the first one
+  return parent.appendChild(child)
+}
+
+function remove(parent, child) {
+  // remove the second parameter(element) from the first one
+  return parent.removeChild(child)
+}
 
 function getPosts() {
-  fetch('https://randomuser.me/api/?results=20')
+  fetch(url)
   .then((response) => response.json()) 
-  .then((posts) => { //posts represents the array of objects
-    generateContacs(posts.results)
+  //posts represents the array of objects
+  .then((posts) => { 
+    let data = posts.results
+
+    return data.forEach(function(x) {
+      let div = createNode('div')
+      let p = createNode('p')
+      let ul = createNode('ul')
+
+      let img = createNode('img')
+      img.src = `${x.picture.large}`
+
+      let email = createNode('li')
+      email.innerHTML = `${x.email}`
+
+      let age = createNode('li')
+      age.innerHTML = `Age: ${x.dob.age}`
+      
+      let button = createNode('button')
+      button.innerText = 'more info'
+      button.addEventListener('click', function() { 
+        if (button.innerText === 'more info') {
+          append(ul, img)
+          append(ul, email)
+          append(ul, age)
+          append(p, ul)
+          button.innerText = 'less info'
+          button.style.background = 'red'
+        } else if (button.innerText === 'less info') {
+          remove(ul, img)
+          remove(ul, email)
+          remove(ul, age)
+          button.innerText = 'more info'
+          button.style.background = 'green'
+        }      
+      })
+
+      p.innerHTML = `${x.name.first} ${x.name.last}: ${x.phone}`
+
+      append(div, p)
+      append(div, button)
+      append(mainDiv, div)     
+    })
   })
 }
-
-function generateContacs(json) {
-  let output = '<h2>Contacts:</h2>'
-
-  console.log('Results array', json)
-
-  json.forEach(function(x) {
-    output += `
-    <div>
-      <h4>${x.name.first} ${x.name.last}</h4>
-      <p>${x.phone}</p>
-      <button class="btn">More Info</button>
-    </div>`
-  })
-  document.getElementById('contacts').innerHTML = output 
-}
-
-
-
-let btn = document.querySelectorAll('button').addEventListener('click', function() {
-  console.log('button clicked')
-})
-
